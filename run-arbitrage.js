9fb5cbe15e412475a297c90846ae0079e5a3ab62
Claudio Canales <ccanales@seegrid.com>
2021-07-06 15:36:32 -0300

    Add folder 11

--- run-arbitrage.js
+++ run-arbitrage.js
@@ -1,4 +1,13 @@
+require('dotenv').config()
 const Web3 = require('web3');
 const web3 = new Web3(
-  new Web3.providers.WebsocketProvider(/* Infura URL goes here */)
+  new Web3.providers.WebsocketProvider(process.env.INFURA_URL)
 );
+
+web3.eth.subscribe('newBlockHeaders')
+  .on('data', async block => {
+    console.log(`New block received. Block # ${block.number}`);
+  })
+  .on('error', error => {
+    console.log(error);
+  });
